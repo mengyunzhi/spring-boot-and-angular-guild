37c4c4235c7904a66284fdbafe951b5ad605dbf3
panjie <3792535@qq.com>
2019-09-30 11:20:44 +0800

    增加测试类

--- api/src/main/java/com/mengyunzhi/springBootStudy/TeacherController.java
+++ api/src/main/java/com/mengyunzhi/springBootStudy/TeacherController.java
@@ -7,7 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeacherController {
 
     @GetMapping
-    public String getAll() {
-        return "get all";
+    public Test getAll() {
+        return new Test(1, "test");
+    }
+
+    class Test{
+        private int id;
+        private String name;
+
+        public Test(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
