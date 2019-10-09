f2476543d1ac56ccc7d2c39476bee6efd43e0ce3
panjie <3792535@qq.com>
2019-10-09 14:55:43 +0800

    匿名函数

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -26,23 +26,15 @@ export class AppComponent implements OnInit {
     /* 后台数据的请求地址，如果变量定义后不再重新赋值，则应该使用const来定义 */
     const url = 'http://localhost:8080/Teacher/';
 
-    /* 定义success方法，用于数据请求成功后回调 */
-    const success = (response) => {
-      console.log(response);
-
-      /*注意：这样写是不正确的，因为this的作用域是本function，而不是本class*/
-      this.teachers = response;
-    };
-
-    /* 定义error方法，用于数据请求失败时被调用 */
-    const error = (response) => {
-      console.log(response);
-      console.error('请求出错');
-    };
-
     /* 使用get方法请求url，请求一旦成功后，将调用传入success方法；如果请求失败，将调用error方法 */
     this.httpClient.get(url)
-      .subscribe(success, error);
+      .subscribe((response: any) => {
+        console.log(response);
+        this.teachers = response;
+      }, (response) => {
+        console.log(response);
+        console.error('请求出错');
+      });
   }
 
 }
