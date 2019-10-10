f7d7f5f556325b2f073196e808ea5e9012467506
panjie <3792535@qq.com>
2019-10-10 08:07:43 +0800

    ng-if与日期管道

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -22,11 +22,10 @@ export class AppComponent implements OnInit {
    * 该方法将在组件准备完毕后被调用
    */
   ngOnInit() {
-    const self = this;
     /* 后台数据的请求地址，如果变量定义后不再重新赋值，则应该使用const来定义 */
     const url = 'http://localhost:8080/Teacher/';
 
-    /* 使用get方法请求url，请求一旦成功后，将调用传入success方法；如果请求失败，将调用error方法 */
+    /* 使用get方法请求url，请求一旦成功后，将调用传入的第一个方法；如果请求失败，将调用传入的第二个方法 */
     this.httpClient.get(url)
       .subscribe((response: any) => {
         console.log(response);
