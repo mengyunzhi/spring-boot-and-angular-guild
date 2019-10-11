197305a6563026e5939370320c5fd8f0fc6f42bc
panjie <3792535@qq.com>
2019-10-11 09:31:21 +0800

    逻辑处理

--- web-app/src/app/teacher-add.component.ts
+++ web-app/src/app/teacher-add.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 /**
  * 教师添加组件
@@ -12,6 +13,34 @@ export class TeacherAddComponent implements OnInit {
   username: string;
   email: string;
   sex: boolean;
+
+
+  constructor(private httpClient: HttpClient) {
+  }
+
   ngOnInit(): void {
+  }
+
+  /**
+   * 将要保存的教师信息提交给后台
+   * 当声明方法为public（此关键字可省略）时，相当于绑定到了V层，V层中可以进行调用或是绑定操作。
+   */
+  public onSubmit(): void {
+    const url = 'http://localhost:8080/Teacher';
+    const teacher = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      sex: this.sex
+    };
+
+    console.log(teacher);
+
+    this.httpClient.post(url, teacher)
+      .subscribe(function() {
+        console.log('添加成功');
+      }, (response) => {
+        console.error('请求发生错误', response);
+      });
   }
 }
