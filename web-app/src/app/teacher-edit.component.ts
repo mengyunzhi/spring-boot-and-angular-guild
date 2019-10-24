71d50c5d0d8b5416d5d8eed7112354fb03fe4a6b
panjie <3792535@qq.com>
2019-10-24 16:45:35 +0800

    temp

--- web-app/src/app/teacher-edit.component.ts
+++ web-app/src/app/teacher-edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {AppComponent} from './app.component';
 
 @Component({
   templateUrl: './teacher-edit.component.html'
@@ -8,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
 export class TeacherEditComponent implements OnInit {
   public teacher: any = {};
 
-  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient, private appComponent: AppComponent) {
   }
 
   ngOnInit(): void {
@@ -26,7 +27,16 @@ export class TeacherEditComponent implements OnInit {
    * 提交表单
    */
   onSubmit(): void {
-    console.log('submit');
+    const id = this.route.snapshot.paramMap.get('id');
+    const url = 'http://localhost:8080/Teacher/' + id;
+    this.httpClient.put(url, this.teacher)
+      .subscribe(() => {
+          console.log('更新成功');
+          this.appComponent.ngOnInit();
+        },
+        () => {
+          console.error(`更新数据时发生错误,url:${url}`);
+        });
   }
 
 }
