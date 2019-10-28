af151a08d22eca60f1d3c9dfb2287cf3584dcef0
panjie <3792535@qq.com>
2019-10-28 08:51:20 +0800

    setp2.4.7

--- web-app/src/app/teacher-edit.component.ts
+++ web-app/src/app/teacher-edit.component.ts
@@ -8,7 +8,7 @@ import {AppComponent} from './app.component';
 })
 export class TeacherEditComponent implements OnInit {
   public teacher: any = {};
-  private url: string;
+  private id: number;
 
   constructor(private route: ActivatedRoute,
               private httpClient: HttpClient,
@@ -20,23 +20,28 @@ export class TeacherEditComponent implements OnInit {
    * 获取与后台对接的URL
    */
   getUrl(): string {
-    if (this.url === undefined) {
-      const id = this.route.snapshot.paramMap.get('id');
-      this.url = 'http://localhost:8080/Teacher/' + id;
-    }
-    return this.url;
+    return 'http://localhost:8080/Teacher/' + this.id;
   }
 
-  ngOnInit(): void {
-    this.route.params.subscribe(data => {
-      console.log(data);
-    });
+  /**
+   * 当路由参数发生变化时，加载教师数据。
+   */
+  load(): void {
+    console.log('加载教师数据');
     this.httpClient.get(this.getUrl())
       .subscribe((data) => {
         this.teacher = data;
       }, () => {
         console.log(`请求 ${this.getUrl()} 时发生错误`);
       });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(data => {
+      console.log('路由参数发生变化，接收通知');
+      this.id = data.id;
+      this.load();
+    });
   }
 
   /**
