9ba8e85e72477669756fd8372a567672827c2704
panjie <3792535@qq.com>
2019-10-29 08:31:19 +0800

    Revert "temp"
    This reverts commit 514d884b

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {TeacherAddComponent} from './teacher/teacher-add.component';
+import {TeacherEditComponent} from './teacher/teacher-edit.component';
+import {TeacherIndexComponent} from './teacher/teacher-index.component';
 
 
 const routes: Routes = [
   {
-    path: 'teacher',
-    loadChildren: () => import('./teacher/teacher.module').then((module) => module.TeacherModule)
+    path: '',
+    component: TeacherIndexComponent
+  },
+  {
+    path: 'add',
+    component: TeacherAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: TeacherEditComponent
   }
 ];
 
--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {TeacherAddComponent} from './teacher/teacher-add.component';
 import {FormsModule} from '@angular/forms';
+import {TeacherEditComponent} from './teacher/teacher-edit.component';
+import {TeacherIndexComponent} from './teacher/teacher-index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TeacherAddComponent,
+    TeacherEditComponent,
+    TeacherIndexComponent
   ],
   imports: [
     BrowserModule,
