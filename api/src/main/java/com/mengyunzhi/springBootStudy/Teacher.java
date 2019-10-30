69656efa849b0f4a2a8541622edaee6a4f0f2d09
panjie <3792535@qq.com>
2019-10-30 09:50:41 +0800

    step3.1

--- api/src/main/java/com/mengyunzhi/springBootStudy/Teacher.java
+++ api/src/main/java/com/mengyunzhi/springBootStudy/Teacher.java
@@ -1,9 +1,17 @@
 package com.mengyunzhi.springBootStudy;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * 教师模型，用于更方便的存储查表后返回的数据
  */
+@Entity
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Boolean sex;
@@ -11,6 +19,9 @@ public class Teacher {
     private String email;
     private Long createTime;
     private Long updateTime;
+
+    public Teacher() {
+    }
 
     public Long getId() {
         return id;
---
+++ api/src/main/java/com/mengyunzhi/springBootStudy/entity/Klass.java
@@ -0,0 +1,36 @@
+package com.mengyunzhi.springBootStudy.entity;
+
+import com.mengyunzhi.springBootStudy.Teacher;
+
+import javax.persistence.*;
+
+@Entity
+public class Klass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Teacher teacher;
+
+    private String name;
+
+    public Klass() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
