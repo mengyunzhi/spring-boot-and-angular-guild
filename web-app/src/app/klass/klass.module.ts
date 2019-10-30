ee40dcc2c43088cecbb95e2552129fbb4d74f262
panjie <3792535@qq.com>
2019-10-30 12:25:49 +0800

    step3.2.1

---
+++ web-app/src/app/klass/klass.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import { IndexComponent } from './index/index.component';
+
+/**
+ * 班级模块
+ */
+@NgModule({  declarations: [IndexComponent]
+})
+export class KlassModule {
+
+}
