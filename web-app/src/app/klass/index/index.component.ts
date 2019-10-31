7176999d7a158ab417469d451a5586a23ea71f70
panjie <3792535@qq.com>
2019-10-31 10:39:17 +0800

    step3.2.3

--- web-app/src/app/klass/index/index.component.ts
+++ web-app/src/app/klass/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.sass']
 })
 export class IndexComponent implements OnInit {
+  private message = '';
+  /*查询参数*/
+  params = {
+    name: ''
+  };
+  /* 班级 */
+  klasses = [
+    {name: '计科1901班', teacher: {id: 1, name: '张三'}},
+    {name: '软件1902班', teacher: {id: 2, name: '李四'}}
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   * 用户点击查询按钮后触发
+   */
+  onQuery(): void {
+    console.log('综合查询');
   }
 
 }
