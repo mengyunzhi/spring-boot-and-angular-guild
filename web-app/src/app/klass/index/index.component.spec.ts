dd4a9e24075a283b24e40c434f5ba6455fdcef0e
panjie <3792535@qq.com>
2019-10-31 10:18:45 +0800

    单元测试中加入f前缀

--- web-app/src/app/klass/index/index.component.spec.ts
+++ web-app/src/app/klass/index/index.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndexComponent } from './index.component';
 
-describe('IndexComponent', () => {
+fdescribe('IndexComponent', () => {
   let component: IndexComponent;
   let fixture: ComponentFixture<IndexComponent>;
 
