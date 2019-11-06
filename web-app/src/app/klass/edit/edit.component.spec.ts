42927d4705a115422035fdd0e0016a282c974e1a
panjie <3792535@qq.com>
2019-11-06 12:29:10 +0800

    step3.4.1

--- web-app/src/app/klass/edit/edit.component.spec.ts
+++ web-app/src/app/klass/edit/edit.component.spec.ts
@@ -2,16 +2,25 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {EditComponent} from './edit.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
 
 describe('klass EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
+  let router: Router;
+  let route: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EditComponent],
       imports: [
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
       ]
     })
       .compileComponents();
@@ -25,5 +34,16 @@ describe('klass EditComponent', () => {
 
   fit('should create', () => {
     expect(component).toBeTruthy();
+    router = TestBed.get(Router);
+    route = TestBed.get(ActivatedRoute);
+    route.params.subscribe(data => {
+      console.log(data);
+    });
+    const debugElement: DebugElement = fixture.debugElement;
+    const teacherIdElement = debugElement.query(By.css('#teacherId'));
+    const teacherIdInput: HTMLInputElement = teacherIdElement.nativeElement;
+    teacherIdInput.value = '2';
+    teacherIdInput.dispatchEvent(new Event('input'));
+    component.onSubmit();
   });
 });
--- web-app/src/app/klass/edit/edit.component.ts
+++ web-app/src/app/klass/edit/edit.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Klass} from '../../norm/entity/Klass';
 
 @Component({
   selector: 'app-edit',
@@ -8,8 +11,27 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class EditComponent implements OnInit {
   formGroup: FormGroup;
+  private url: string;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private httpClient: HttpClient) {
+  }
+
+  private getUrl(): string {
+    return this.url;
+  }
+
+  /**
+   * 加载要编辑的班级数据
+   */
+  loadData(): void {
+    this.httpClient.get(this.getUrl())
+      .subscribe((klass: Klass) => {
+        this.formGroup.setValue({name: klass.name, teacherId: klass.teacher.id});
+      }, () => {
+        console.error(`${this.getUrl()}请求发生错误`);
+      });
   }
 
   ngOnInit() {
@@ -17,6 +39,31 @@ export class EditComponent implements OnInit {
       name: new FormControl(),
       teacherId: new FormControl()
     });
+    this.route.params.subscribe((param: { id: number }) => {
+      this.setUrlById(param.id);
+      this.loadData();
+    });
   }
 
+  /**
+   * 用户提交时执行的操作
+   */
+  onSubmit(): void {
+    const data = {
+      name: this.formGroup.value.name,
+      teacher: {id: this.formGroup.value.teacherId}
+    };
+    this.httpClient.put(this.getUrl(), data)
+      .subscribe(() => {
+        this.router.navigateByUrl('', {relativeTo: this.route});
+      }, () => {
+        console.error(`在${this.getUrl()}上的PUT请求发生错误`);
+      });
+  }
+
+
+  private setUrlById(id: number): void {
+    this.url = `http://localhost:8080/Klass/${id}`;
+  }
 }
+
