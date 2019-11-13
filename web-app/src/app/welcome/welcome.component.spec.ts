2dd7eabb8a3c02f2279eac0e01dbdefb68b86328
panjie <3792535@qq.com>
2019-11-13 16:28:14 +0800

    step4.4

--- web-app/src/app/welcome/welcome.component.spec.ts
+++ web-app/src/app/welcome/welcome.component.spec.ts
@@ -19,7 +19,7 @@ describe('WelcomeComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
