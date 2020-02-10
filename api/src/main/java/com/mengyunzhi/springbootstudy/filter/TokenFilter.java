1b972c221e4a1bfe1ebb69c7d088f75b5ef04e0d
panjie <3792535@qq.com>
2020-02-10 18:29:38 +0800

    step5.1.0

--- api/src/main/java/com/mengyunzhi/springbootstudy/filter/TokenFilter.java
+++ api/src/main/java/com/mengyunzhi/springbootstudy/filter/TokenFilter.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.UUID;
 
 /**
  * 令牌过滤器
@@ -21,22 +19,11 @@ import java.util.UUID;
 @WebFilter
 public class TokenFilter extends HttpFilter {
     private final static Logger logger = LoggerFactory.getLogger(TokenFilter.class);
-    private String TOKEN_KEY = "auth-token";
-    /** 存储已分发过的令牌 */
-    private HashSet<String> tokens = new HashSet<>();
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 获取 header中的token
-        String token = request.getHeader(this.TOKEN_KEY);
-        logger.info("获取到的token为" + token);
-
-        // 有效性判断
-        if (!this.validateToken(token)) {
-            // 如果无效则分发送的token
-            token = this.makeToken();
-            logger.info("原token无效，发布的新的token为" + token);
-        }
+        // 获取 header中的token并做有效性验证
+        // 如果无效则分发送的token
 
         logger.info("在控制器被调用以前执行");
 
@@ -46,29 +33,5 @@ public class TokenFilter extends HttpFilter {
         logger.info("在控制器被调用以后执行");
 
         // 为http响应加入新token后返回
-    }
-
-    /**
-     * 生成token
-     * 将生成的token存入已分发的tokens中
-     * @return token
-     */
-    private String makeToken() {
-        String token = UUID.randomUUID().toString();
-        this.tokens.add(token);
-        return token;
-    }
-
-    /**
-     * 验证token的有效性
-     * @param token token
-     * @return
-     */
-    private boolean validateToken(String token) {
-        if (token == null) {
-            return false;
-        }
-
-        return this.tokens.contains(token);
     }
 }
