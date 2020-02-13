17c8d5aec5c879a9e9aec7f7da00b8461c815fd4
panjie <3792535@qq.com>
2020-02-13 18:56:22 +0800

    step5.2.1

--- web-app/src/app/core/auth-token-interceptor.ts
+++ web-app/src/app/core/auth-token-interceptor.ts
@@ -1,13 +1,25 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {CacheService} from '../service/cache.service';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const reqClone = req.clone({
-      setHeaders: {'auth-token': 'af1c0c77-67d0-4ec2-8321-2f88e32f76af'}
+      setHeaders: {'auth-token': CacheService.getAuthToken()}
     });
-    return next.handle(reqClone);
+    return next.handle(reqClone).pipe(map((httpEvent) => {
+      if (httpEvent instanceof HttpResponse) {
+        const httpResponse = httpEvent as HttpResponse<any>;
+        const authToken = httpResponse.headers.get('auth-token');
+        CacheService.setAuthToken(authToken);
+      }
+
+      return httpEvent;
+    }));
   }
 }
+
+
