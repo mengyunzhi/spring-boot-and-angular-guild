6e675458ba3e0574895a9f44e4277f53fdab1886
panjie <3792535@qq.com>
2020-02-14 15:27:48 +0800

    语法

--- api/src/test/java/com/mengyunzhi/springbootstudy/controller/TeacherControllerTest.java
+++ api/src/test/java/com/mengyunzhi/springbootstudy/controller/TeacherControllerTest.java
@@ -1,6 +1,5 @@
 package com.mengyunzhi.springbootstudy.controller;
 
-import com.jayway.jsonpath.JsonPath;
 import com.mengyunzhi.springbootstudy.service.TeacherService;
 import net.minidev.json.JSONObject;
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
@@ -8,7 +7,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
