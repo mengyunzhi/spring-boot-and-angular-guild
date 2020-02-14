364227183dc409f7bcdf1efb077739e7407c72f0
panjie <3792535@qq.com>
2020-02-14 10:59:50 +0800

    temp

--- web-app/src/app/login/login.component.spec.ts
+++ web-app/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormTest} from '../testing/FormTest';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +24,29 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('表单绑定', () => {
+    // 设置C层的值后重新渲染V层
+    component.formGroup.get('username').setValue('testUsername');
+    component.formGroup.get('password').setValue('testPassword');
+    fixture.detectChanges();
+
+    // 获取V层的值
+    const usernameValue = FormTest.getInputValueByFixtureAndCss(fixture, '#username');
+    const passwordValue = FormTest.getInputValueByFixtureAndCss(fixture, '#password');
+
+    // 断言CV两层的值相等
+    expect(usernameValue).toEqual('testUsername');
+    expect(passwordValue).toEqual('testPassword');
+  });
+
+  fit('点击提交按钮', () => {
+    spyOn(component, 'onSubmit');
+    FormTest.clickButton(fixture, 'button');
+
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
--- web-app/src/app/login/login.component.ts
+++ web-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
+  formGroup: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.formGroup = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    });
   }
 
+  onSubmit() {
+    const username = this.formGroup.get('username').value;
+    const username = this.formGroup.get('username').value;
+
+  }
 }
