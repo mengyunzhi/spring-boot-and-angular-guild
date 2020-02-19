b44cf6e42b6893cffadb4e1757b86caf9c9a34ff
panjie <3792535@qq.com>
2020-02-19 18:27:42 +0800

    5.2.8

---
+++ api/src/main/java/com/mengyunzhi/springbootstudy/interceptor/AuthInterceptor.java
@@ -0,0 +1,33 @@
+package com.mengyunzhi.springbootstudy.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author panjie
+ */
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        logger.info("执行拦截器preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           @Nullable ModelAndView modelAndView) throws Exception {
+        logger.info("执行拦截器postHandle");
+    }
+
+}
