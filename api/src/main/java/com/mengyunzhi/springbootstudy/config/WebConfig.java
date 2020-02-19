08300ccd057f5a51be786b69ad977db2f1a329db
panjie <3792535@qq.com>
2020-02-19 18:33:21 +0800

    配置拦截器

--- api/src/main/java/com/mengyunzhi/springbootstudy/config/WebConfig.java
+++ api/src/main/java/com/mengyunzhi/springbootstudy/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.mengyunzhi.springbootstudy.config;
 
+import com.mengyunzhi.springbootstudy.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH")
                 .exposedHeaders("auth-token");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor());
     }
 }
